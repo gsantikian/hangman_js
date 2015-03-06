0b3cb8c0c81d237c6fa5ed0f049bdeb54bee43c3
Greg Santikian <gsantikian@gmail.com>
2015-03-06 14:34:00 -0500

    Adds some variables and requires sget

--- hangman.js
+++ hangman.js
@@ -1,7 +1,20 @@
-var words = ["detroit", "chicago"];
+var sget = require('sget');
+
+var words = ["detroit", "chicago", "boston", "banana", "car"];
 var word = words[Math.floor(Math.random() * words.length)];
 var wordLength = word.length;
+var letters = word.split('');
+
+var numberOfGuesses = 6;
+var guessedLetters = [];
 
 console.log(word);
 console.log(wordLength);
+console.log(letters);
 
+
+
+// should let user guess a limited number of times (6?)
+// should keep track of the letters the user has already guessed
+// display guesses on request
+// offer up 2 1-letter hints on request
