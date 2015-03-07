4a746fc37df40bca601a0005ece2c0bf3dc9ab68
Greg Santikian <gsantikian@gmail.com>
2015-03-07 18:01:57 -0500

    checks userInput and branches accordingly

--- hangman.js
+++ hangman.js
@@ -2,6 +2,7 @@ var sget = require('sget');
 
 var words = ["detroit", "chicago", "boston", "hello", "banana", "whatever"];
 var word = words[Math.floor(Math.random() * words.length)];
+var re = /[a-z]/;
 
 function Game(word) {
   this.word = word;
@@ -33,8 +34,16 @@ Game.prototype.checkDuplicateGuess = function(guess) {
 game = new Game(word);
 
 console.log("Welcome to Hangman!");
-game.storeGuess('b');
-console.log(game.guesses);
 
-var userInput = sget("Enter a letter or type 'hint' or 'guesses':");
-
+while (true) {
+  var userInput = sget("Enter a letter or type 'hint' or 'guesses':").trim().toLowerCase();
+  if (userInput === 'hint') {
+    console.log("HINT!!!!");
+  } else if (userInput === 'guesses'){
+    console.log("GUESSES!!!");
+  } else if (userInput.length === 1 && re.test(userInput)) {
+    console.log("LETTERS!!!");
+  } else {
+    console.log("Invalid input. Please try again.");
+  }
+}
