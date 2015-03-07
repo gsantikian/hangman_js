fcbe086bdad73ecd424debb5442be81d8d33a69d
Greg Santikian <gsantikian@gmail.com>
2015-03-07 17:44:31 -0500

    Adds game methods

--- hangman.js
+++ hangman.js
@@ -3,12 +3,13 @@ var sget = require('sget');
 var words = ["detroit", "chicago", "boston", "hello", "banana", "whatever"];
 var word = words[Math.floor(Math.random() * words.length)];
 
-var letters = word.split('');
-
 function Game(word) {
   this.word = word;
+  this.wordLength = this.word.length;
+  this.letters = this.word.split('');
   this.guessesLeft = 6;
   this.hintsLeft = 2;
+  this.guesses = [];
 }
 
 Game.prototype.decrementGuess = function() {
@@ -17,13 +18,23 @@ Game.prototype.decrementGuess = function() {
 
 Game.prototype.decrementHint = function() {
   this.hintsLeft -= 1;
-}
+};
+
+Game.prototype.storeGuess = function(guess) {
+  this.guesses.push(guess);
+};
+
+Game.prototype.checkDuplicateGuess = function(guess) {
+  if (this.guesses.indexOf(guess) < 0) {
+    console.log("You have already guessed that letter. Try again.");
+  }
+};
 
 game = new Game(word);
 
-console.log(game.word);
-console.log(game.guessesLeft);
-game.decrementGuess();
-console.log(game.guessesLeft);
-game.decrementHint();
-console.log(game.hintsLeft);
+console.log("Welcome to Hangman!");
+game.storeGuess('b');
+console.log(game.guesses);
+
+var userInput = sget("Enter a letter or type 'hint' or 'guesses':");
+
